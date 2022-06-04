241f0a5d984ea9ed0330ff20d66cabd760cff461
john kaiser <52598487+johnkaizer@users.noreply.github.com>
2022-06-04 13:31:25 +0300

    code for second activity

--- app/src/main/java/com/example/firebaselogin/Home.kt
+++ app/src/main/java/com/example/firebaselogin/Home.kt
@@ -1,11 +1,38 @@
 package com.example.firebaselogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firebaselogin.databinding.ActivityHomeBinding
+import com.example.firebaselogin.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Home : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var user: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //getting logged  user instance
+        user = FirebaseAuth.getInstance()
+        if (user.currentUser != null) {
+            user.currentUser?.let {
+                binding.userEmail.text = it.email
+            }
+        }
+
+        binding.btnSignOut.setOnClickListener {
+            user.signOut()
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java
+
+                )
+            )
+            finish()
+        }
     }
 }
